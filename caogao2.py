eb4103946ca308769604ace40379958a1b2408c2
135792468abc <133783996+135792468abc@users.noreply.github.com>
2023-08-04 14:26:21 +0800

    Add files via upload

---
+++ caogao2.py
@@ -0,0 +1,23 @@
+import functhion
+import time
+
+print ("take off ------ 3m")
+functhion.arm_and_takeoff(3)
+time.sleep(10)
+functhion.goto_position_target_local_ned(-10,0,-3)
+time.sleep(10)
+
+
+functhion.goto_position_target_local_ned(0,0,-3)
+time.sleep(10)
+
+
+print ("mode  RTL")
+vehicle.mode = VehicleMode("RTL")
+time.sleep(10)
+vehicle.close()
+
+
+
+
+ 
